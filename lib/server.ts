eaed0c07be73673de2a9da7b02f2a8c005120a6f
Paulus Esterhazy <pesterhazy@gmail.com>
2024-01-07 23:39:11 +0100

    WIP

--- lib/server.ts
+++ lib/server.ts
@@ -1,9 +1,13 @@
-import express from "express";
+import express, { Request, Response } from "express";
 
 const defaultPort = 4004;
 
 export function run(port = defaultPort) {
   let app = express();
+
+  app.get("/", (req: Request, res: Response) => {
+    res.send("Express + TypeScript Server");
+  });
 
   return app.listen(port);
 }
