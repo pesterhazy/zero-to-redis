19d33a158013d01697c61514832c6faa512c114e
Paulus Esterhazy <pesterhazy@gmail.com>
2024-01-07 15:34:43 +0100

    WIP

--- test/main.test.ts
+++ test/main.test.ts
@@ -5,31 +5,29 @@ import assert from "node:assert/strict";
 
 async function me() {
   let body = {
-    query:
-      "query me {\n" +
-      "  me {\n" +
-      "    ...UserFields\n" +
-      "  }\n" +
-      "}\n" +
-      "\n" +
-      "fragment UserFields on User {\n" +
-      "  id\n" +
-      "  email\n" +
-      "  name\n" +
-      "}\n",
+    query: `
+query me {
+  me {
+    ...UserFields
+  }
+}
+
+fragment UserFields on User {
+  id
+  email
+  name
+}
+`,
     operationName: "me",
   };
-  let response = await fetch(
-    "https://backboard.railway.app/graphql/internal?q=me",
-    {
-      body: JSON.stringify(body),
-      headers: {
-        authorization: "Bearer " + process.env.RAILWAY_API_TOKEN,
-        "content-type": "application/json",
-      },
-      method: "POST",
+  let response = await fetch("https://backboard.railway.app/graphql/v2", {
+    body: JSON.stringify(body),
+    headers: {
+      authorization: "Bearer " + process.env.RAILWAY_API_TOKEN,
+      "content-type": "application/json",
     },
-  );
+    method: "POST",
+  });
 
   if (!response.ok) throw "HTTP request failed: " + response.status;
 
