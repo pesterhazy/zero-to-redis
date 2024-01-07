530cb20dbe63af1dd08a59172160ec42a4396e87
Paulus Esterhazy <pesterhazy@gmail.com>
2024-01-07 15:38:45 +0100

    WIP

--- test/main.test.ts
+++ test/main.test.ts
@@ -3,9 +3,10 @@
 import test from "node:test";
 import assert from "node:assert/strict";
 
-async function me() {
-  let body = {
-    query: `
+class RailwayClient {
+  async me() {
+    let body = {
+      query: `
 query me {
   me {
     ...UserFields
@@ -18,23 +19,25 @@ fragment UserFields on User {
   name
 }
 `,
-    operationName: "me",
-  };
-  let response = await fetch("https://backboard.railway.app/graphql/v2", {
-    body: JSON.stringify(body),
-    headers: {
-      authorization: "Bearer " + process.env.RAILWAY_API_TOKEN,
-      "content-type": "application/json",
-    },
-    method: "POST",
-  });
+      operationName: "me",
+    };
+    let response = await fetch("https://backboard.railway.app/graphql/v2", {
+      body: JSON.stringify(body),
+      headers: {
+        authorization: "Bearer " + process.env.RAILWAY_API_TOKEN,
+        "content-type": "application/json",
+      },
+      method: "POST",
+    });
 
-  if (!response.ok) throw "HTTP request failed: " + response.status;
+    if (!response.ok) throw "HTTP request failed: " + response.status;
 
-  let json = await response.json();
-  return json.data.me.name;
+    let json = await response.json();
+    return json.data.me.name;
+  }
 }
 
 test(async function () {
-  assert.equal("Paulus Esterhazy", await me());
+  let client = new RailwayClient();
+  assert.equal("Paulus Esterhazy", await client.me());
 });
